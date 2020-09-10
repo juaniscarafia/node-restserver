d461e173e08f1f77fe5eba0d21f11e7b5ba9eeea
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2020-09-10 16:07:55 -0300

    Variables de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB =
-        "mongodb+srv://fisionito:DRW0Vs36GD6WfE53@cluster0.mq8dh.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
