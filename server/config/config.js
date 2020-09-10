899de1d03475e7da54781d0944ace53913fc9d5e
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2020-09-10 15:58:24 -0300

    URLS conexion bd

--- server/config/config.js
+++ server/config/config.js
@@ -12,10 +12,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 // BASE DE DATOS
 // ======================================
 let urlDB;
-// if (process.env.NODE_ENV === "dev") {
-// urlDB = "mongodb://localhost:27017/cafe";
-// } else {
-urlDB =
-    "mongodb+srv://fisionito:DRW0Vs36GD6WfE53@cluster0.mq8dh.mongodb.net/cafe";
-// }
+if (process.env.NODE_ENV === "dev") {
+    urlDB = "mongodb://localhost:27017/cafe";
+} else {
+    urlDB =
+        "mongodb+srv://fisionito:DRW0Vs36GD6WfE53@cluster0.mq8dh.mongodb.net/cafe";
+}
 process.env.URLDB = urlDB;
